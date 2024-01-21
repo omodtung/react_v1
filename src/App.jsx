58620f3bc24907666da6c7dceb8f1d751f43d49c
omodtung <dothedung131@gmail.com>
2024-01-21 17:59:46 +0700

    import and Export

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+import Gallery from './Gallery';
+//your first ComPonent 
+// function Profile() {
+//  return (
+// <img src= "https://i.imgur.com/MK3eW3As.jpg"
+//   alt='katherie faker'
+// />
+
+//  );
+// }
+
+function App ()
+{
+  return (
+<Gallery/>
+  );
+}
+
+export default App
---
+++ src/Gallery.jsx
@@ -0,0 +1,16 @@
+import Profile from "./Profile"
+
+
+export default function Gallery ()
+{
+    return (
+
+        <section>
+            <h1> Importing and ExportTing </h1>
+
+            <Profile></Profile>
+            <Profile></Profile>
+            <Profile></Profile>
+        </section>
+    )
+}
