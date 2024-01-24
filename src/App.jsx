eb3900b8d729b44f9a48ac09cd8667fe5e11bbe7
omodtung <dothedung131@gmail.com>
2024-01-24 14:27:15 +0700

    passing props to a component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 
 import './App.css'
 import Gallery from './Gallery';
+
+import { getImageUrl  } from './utils';
 //your first ComPonent 
 // function Profile() {
 //  return (
@@ -11,12 +13,99 @@ import Gallery from './Gallery';
 
 //  );
 // }
+const person = 
+{
+  name : 'greo_ri_o Just a Username',
+  theme:
+  {
+    backgroundColor : 'black',color:'pink'
+    
+
+  }
+};
+
+function card ({children})
+{
+  return (
+    <div className = "card">
+{children}
+    </div>
+  )
+}
+function Profile ()
+{
+  return (
+   < card
+   >
+   
+   <Avatar
+      size={100}
+      person={
+        {
+          name :'From Person In Profile',
+          imageId:'123'
+        }
+      }
+      />
+
+   </card>
+
+      
+
+    
+  );
+}
+
+
+function Avatar ({person,size})
+{
+
+
+
+  return  (
+
+    
+<>
+{/* person =Prototype.name; */}
+
+  <img
+  className='avatar'
+  src={getImageUrl(person)}
+  alt={person.name}
+  width={size}
+height={size}
+   />  
+  
+
+  
+
+   </>
+  
+  );
+
+}
+// conditional Rendering
+function item({name, isPacked})
+{
+  return(
+    <li className='item'>
+{name} {isPacked && 'x'}
+    </li>
+  );
+}
+function Pac
 
 function App ()
 {
-  return (
-<Gallery/>
-  );
+//   return (
+// <Gallery/>
+//   );
+
+return (
+<>
+<Profile></Profile>
+</>
+);
 }
 
 export default App
